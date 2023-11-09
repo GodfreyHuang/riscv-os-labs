d611ea2b7b6df36f841a825a533b69f5602abbf0
Ancacion <joker103186@gmail.com>
2023-11-09 21:39:59 +0800

    try to using struct to contain task datastructure but cannot use

--- 03-ContextSwitch/src/main.c
+++ 03-ContextSwitch/src/main.c
@@ -7,7 +7,7 @@ uint8_t task0_stack[STACK_SIZE];
 struct context ctx_os;
 struct context ctx_task;
 
-void user_task()
+void user_task0()
 {
     os_puts("Task0: Context Switch Success!\n");
     while(1) {}
@@ -17,7 +17,13 @@ int main(int argc, char *argv[])
 {
     os_puts("riscv os operate!\n");
 
-    ctx_task.ra = (reg_t) user_task;
+    // struct task_handler *th = (struct task_handler)malloc(sizeof(struct task_handler));
+    
+    // th->ctx_task.ra = (reg_t) user_task0;
+    // th->ctx_task.sp = (reg_t) &(th->task0_stack[STACK_SIZE - 1]);
+    // sys_switch(&(th->ctx_os), &(th->ctx_task));
+
+    ctx_task.ra = (reg_t) user_task0;
     ctx_task.sp = (reg_t) &task0_stack[STACK_SIZE - 1];
     sys_switch(&ctx_os, &ctx_task);
 
