009e7731335989fd7b5f71217638781f6c318ec4
GodfreyHuang <godfreyhuang1031@gmail.com>
2023-12-13 11:12:49 +0800

    Update basic timer and multitasking in 06

--- 06-Preemptive/include/os.h
+++ 06-Preemptive/include/os.h
@@ -3,5 +3,21 @@
 
 #include "riscv.h"
 #include "lib.h"
+#include "task.h"
+
+//context switch
+extern void sys_switch();
+
+//time counter
+extern void sys_timer();
+extern void timer_handler();
+extern void timer_init();
+
+//user task
+extern void user_init();
+
+//os
+extern void os_kernel();
+extern void os_start();
 
 #endif
--- 06-Preemptive/src/main.c
+++ 06-Preemptive/src/main.c
@@ -1,8 +1,31 @@
+#include "riscv.h"
 #include "os.h"
+
+void os_kernel()
+{
+    task_os();
+}
+
+void os_start()
+{
+    user_init();
+    timer_init();
+}
 
 int main(int argc, char *argv[])
 {
     os_puts("Init riscv os!\n");
-    while(1) {}
+    os_start();
+
+    int current_task = 0;
+    while (1) 
+    {
+        os_puts("OS: Activate next task\n");
+        task_go(current_task);
+        os_puts("OS: Back to OS\n");
+        current_task = (current_task + 1) % taskTop;
+        os_puts("\n");
+    }
+    
     return 0;
 }
